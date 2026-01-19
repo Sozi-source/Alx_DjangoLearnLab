cfa5321db589e694939a8cb2cc0990cc55bc953f
Wilfred Osozi <osoziw@gmail.com>
2026-01-20 01:53:26 +0600

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -29,10 +29,5 @@ urlpatterns = [
         
         ),
 
-   path(
-       'logout/', 
-       LogoutView.as_view(
-           template_name='relationship_app/logout.html'
-        ), 
-        name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
