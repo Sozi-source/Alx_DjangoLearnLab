cc0a8acc313909741ff28abd8a67544511f42560
Wilfred Osozi <osoziw@gmail.com>
2026-01-20 01:46:19 +0600

    added user authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,41 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailedView
+from .views import list_books, LibraryDetailView, register
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('list_books/', views.list_books, name='list_books'),
-    path('libraryDetails/<int:pk>/', views.LibraryDetailView.as_view(), name='library_details')
+    path(
+        'list_books/', 
+         views.list_books, 
+         name='list_books'
+         ),
+
+    path(
+        'libraryDetails/<int:pk>/', 
+        views.LibraryDetailView.as_view(), 
+        name='library_details'
+        ),
+
+    path(
+        'login/', 
+        LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login'
+        ),
+
+    path(
+        'register/', 
+        views.register,
+        name = 'register'
+        
+        ),
+
+    path(
+        'logout/', 
+        LogoutView.as_view(
+        template_name='relationship_app/logout.html',
+        next_page = 'login',
+        http_method_names = ['get', 'post']
+        ),
+        name='logout'
+        ),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Library, Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 # Create your views here.
 def list_books(request):
@@ -14,3 +21,17 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+# user registration
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Registration successful! You can now log in.')
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render (request, 'relationship_app/register.html', {'form':form})
