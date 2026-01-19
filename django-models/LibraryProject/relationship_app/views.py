d0da6722ab428bac6cc0509917e46b51b7aa0add
Wilfred Osozi <osoziw@gmail.com>
2026-01-20 00:01:15 +0600

    updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
 
-    return render(request, 'books/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 
 class LibraryDetailedView(DetailView):
