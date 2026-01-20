534f84b95bd96a131d4f270bf9baa29023b7182c
Wilfred Osozi <osoziw@gmail.com>
2026-01-21 02:18:31 +0600

    updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -40,7 +40,7 @@ def register(request):
 
 # admin
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role =='ADMIN'
+    return user.is_authenticated and user.profile.role =='ADMIN'
 
 @user_passes_test(is_admin)
 def admin_view(request):
