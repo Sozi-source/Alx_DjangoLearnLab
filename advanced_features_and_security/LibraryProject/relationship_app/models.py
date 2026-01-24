0118254ed34206f25953fe21858cd9d8f00501cc
Wilfred Osozi <osoziw@gmail.com>
2026-01-24 16:52:20 +0300

    updated

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,45 +1,13 @@
+# relationship_app/models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.conf import settings  # Add this import
+from django.conf import settings
+
+# Import CustomUser from bookshelf app
+from bookshelf.models import CustomUser
 
 # Create your models here.
-
-# Custom User Manager
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(username, email, password, **extra_fields)
-
-# Custom User Model
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-    
-    # Use your custom manager
-    objects = CustomUserManager()
-    
-    def __str__(self):
-        return self.username
-
-
 class Author(models.Model):
     name=models.CharField(max_length=100)
 
@@ -89,9 +57,8 @@ class UserProfile(models.Model):
         (MEMBER, 'Member'),
     ]
     
-    # IMPORTANT: Change from User to settings.AUTH_USER_MODEL
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,  # Changed from User
+        settings.AUTH_USER_MODEL,  # This will now point to bookshelf.CustomUser
         on_delete=models.CASCADE, 
         related_name='profile'
     )
