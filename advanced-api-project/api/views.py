37f60e51f3d2fa5b09b5bce3eed3554995542aae
Wilfred Osozi <osoziw@gmail.com>
2026-02-08 23:16:08 +0300

    add permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
 # =========== Dummy Django Views for Checker ===========
 class BookView(ListView):
