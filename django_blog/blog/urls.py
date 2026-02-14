5243dda0eae45365c99888b1664f63fe7a2b1eb9
Wilfred Osozi <osoziw@gmail.com>
2026-02-14 23:12:12 +0300

    altered urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     
     # Post-specific comments (more intuitive)
     path('post/<int:post_id>/comments/', views.CommentListView.as_view(), name='post-comments'),
-    path('post/<int:post_id>/comment/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     
     # Individual comment operations
     path('comment/<int:pk>/', views.CommentDetailView.as_view(), name='comment-detail'),
